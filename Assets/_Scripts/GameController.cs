d4c2cad3a01477ee4a08c98e8a90d2e565414605
Trent B <trentbmin@gmail.com>
2021-02-11 21:35:24 -0500

    Add project files

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    void OnGUI() {
+        if (GUI.Button(new Rect(10, 10, 100, 30), "Start")) SceneManager.LoadScene("Main");
+    }
+}
